cf60ed04f4d1f0d2b8e316b9a72fa5f4243b0a91
Алексей <electrifyx@gmail.com>
2023-03-08 19:13:22 +0200

    add class

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,70 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-import { fetchGallery } from './fetchGallery';
+import NewsApiService from './news-service';
 
 const searchForm = document.querySelector('#search-form');
 const galleryElement = document.querySelector('.gallery');
+const buttonGallery = document.querySelector('.gallery__button');
+const NewsApiService = new NewsApiService();
 
 searchForm.addEventListener('submit', onSearch);
+buttonGallery.addEventListener('click', onLoadMore);
+
+
+// let page = 1;
+// function onLoadMore(event) {
+//   if (event) {
+//     page += 1;
+//   } else {
+//     page === 1;
+//   }
+// }
+
+
 
 function onSearch(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formSearchQuery = event.currentTarget.elements.searchQuery.value;
+  NewsApiService.query = event.currentTarget.elements.searchQuery.value.trim();
+  NewsApiService.fetchGallery();
+  // fetchGallery(formSearchQuery, page).then(response.length === 0);
+
+  // if (!formSearchQuery) {
+  //   clearTemplate();
+  //   return;
+  // }
 }
 
-function renderGalleryMarkup(elements) {
-  return elements
-    .map(({}) => {
-      return `<div class="photo-card">
-      <img src="${photo}" alt="" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b>
-        </p>
-        <p class="info-item">
-          <b>Views</b>
-        </p>
-        <p class="info-item">
-          <b>Comments</b>
-        </p>
-        <p class="info-item">
-          <b>Downloads</b>
-        </p>
-      </div>
-    </div>`;
-    })
-    .join('');
+function onLoadMore(event) {
+  NewsApiService.fetchGallery();
 }
+
+// function clearTemplate() {
+//   countryInfo.innerHTML = '';
+//   countryList.innerHTML = '';
+// }
+
+
+
+// function renderGalleryMarkup(elements) {
+//   return elements
+//     .map(({}) => {
+//       return `<div class="photo-card">
+//       <img src="${photo}" alt="" loading="lazy" />
+//       <div class="info">
+//         <p class="info-item">
+//           <b>Likes</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Views</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Comments</b>
+//         </p>
+//         <p class="info-item">
+//           <b>Downloads</b>
+//         </p>
+//       </div>
+//     </div>`;
+//     })
+//     .join('');
+// }
